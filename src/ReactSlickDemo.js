3b909a604b53bd79a1f4311fccc99274fa7e7b67
Chiara Yen <chiyen.chiara@gmail.com>
2017-08-31 16:00:02 +0800

    add slick number two

--- src/ReactSlickDemo.js
+++ src/ReactSlickDemo.js
@@ -3,36 +3,74 @@ import Slider from 'react-slick';
 import './ReactSlickDemo.css';
 
 export default class ReactSlickDemo extends Component {
-   componentDidMount() {
-      document.addEventListener('keydown', e =>{
-      if ((e.keyCode) === 37) {
-         this.slider.slickPrev();
-      } else if ((e.keyCode) === 39) {
-        this.slider.slickNext();
-       } else {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFirstLine: true,
+      isSecondLine: false,
+    }
+  }
 
-       }
-     });
-   }
+  componentDidMount() {
+    const { isFirstLine, isSecondLine } = this.state;
+    const focusBox = document.getElementById('focus-box');
+    document.addEventListener('keydown', (e) =>{
+      if (e.keyCode === 37) {
+        isFirstLine && !isSecondLine ? this.slider.slickPrev() : this.slider2.slickPrev();
+      } else if (e.keyCode === 39) {
+        isFirstLine && !isSecondLine ? this.slider.slickNext() : this.slider2.slickNext();
+      } else if (e.keyCode === 40) {
+        if (!focusBox.classList.contains('second')) {
+          focusBox.classList.add('second');
+        }
+        this.setState({
+          isFirstLine: false,
+          isSecondLine: true,
+        })
+      }  else if (e.keyCode === 38) {
+        if (focusBox.classList.contains('second')) {
+          focusBox.classList.remove('second');
+        }
+        this.setState({
+          isFirstLine: true,
+          isSecondLine: false,
+        })
+      }
+    });
+  }
 
-   render() {
+  render() {
     var settings = {
-       infinite: true,
-       speed: 150,
-       slidesToShow: 2,
-       arrows: false,
-       useCSS: true,
-     }
-     return (
+      infinite: true,
+      speed: 150,
+      slidesToShow: 8,
+      arrows: false,
+      useCSS: true,
+    }
+    return (
       <div className='container'>
-        <Slider ref={n => this.slider = n } {...settings}>
-          <div><img src='http://placekitten.com/g/400/200' alt="1" /></div>
-          <div><img src='http://placekitten.com/g/400/200' alt="2" /></div>
-          <div><img src='http://placekitten.com/g/400/200' alt="3" /></div>
-          <div><img src='http://placekitten.com/g/400/200' alt="4" /></div>
-         </Slider>
-         <div className='focus' />
-       </div>
-     );
-   }
+        <Slider
+          {...settings}
+          ref={n => this.slider = n }
+          className="slider-bar"
+        >
+          <div><img src='http://placekitten.com/g/200/200' alt="1" /></div>
+          <div><img src='http://placekitten.com/g/200/200' alt="2" /></div>
+          <div><img src='http://placekitten.com/g/200/200' alt="3" /></div>
+          <div><img src='http://placekitten.com/g/200/200' alt="4" /></div>
+        </Slider>
+        <Slider
+          {...settings}
+          ref={n => this.slider2 = n }
+          className="slider-bar"
+        >
+          <div><img src='http://placekitten.com/g/200/200' /></div>
+          <div><img src='http://placekitten.com/g/200/200' /></div>
+          <div><img src='http://placekitten.com/g/200/200' /></div>
+          <div><img src='http://placekitten.com/g/200/200' /></div>
+        </Slider>
+        <div id="focus-box" className='focus' />
+      </div>
+    );
+  }
 }
