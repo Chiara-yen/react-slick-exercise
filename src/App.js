5f68eb79c824894d21e641eaa6f29b57196cd9d7
Chiara Yen <chiyen.chiara@gmail.com>
2017-09-02 15:34:31 +0800

    update title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React Slick Demo</h2>
+          <h2 className="App-title">Welcome to React Slick Demo</h2>
+          <p>press key &larr; &uarr; &rarr; &darr;</p>
         </div>
         <ReactSlickDemo />
       </div>
