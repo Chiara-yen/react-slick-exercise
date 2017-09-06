b24aa71ef1abbeaaa9eba85f0745a73c3f7b6adc
Chiara Yen <chiyen.chiara@gmail.com>
2017-09-06 17:06:36 +0800

    clean up code and add getFocusBoxTop function

--- src/ReactSlickDemo.js
+++ src/ReactSlickDemo.js
@@ -12,6 +12,8 @@ const KeyMap = {
 const CarouselNames = [ 'slider', 'slider2', 'slider3' ];
 const MaxIndex = CarouselNames.length - 1;
 
+const imgHeight = 200;
+
 const makefakeData = (name) => Array(8).fill(1).map((it, i) => {
   return {
     id: i,
@@ -29,49 +31,64 @@ export default class ReactSlickDemo extends Component {
   }
 
   componentDidMount() {
-    const focusBox = document.getElementById('focus-box');
-    document.addEventListener('keydown', (e) => {
+    this.focusBox = document.getElementById('focus-box');
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
 
-      switch (e.keyCode) {
-        case KeyMap.left:
-          this[this.state.isActived].slickPrev();
-          break;
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
 
-        case KeyMap.right:
-          this[this.state.isActived].slickNext();
-          break;
+  handleKeyDown = (e) => {
+    const currIndex = CarouselNames.findIndex(name => name === this.state.isActived);
 
-        case KeyMap.up:
-          const currIndexUp = CarouselNames.findIndex(name => name === this.state.isActived);
-          const nextIndexUp = currIndexUp - 1 < 0 ? 0 : currIndexUp - 1;
-          focusBox.classList = `focus ${CarouselNames[nextIndexUp]}`;
-          this.setState({
-            isActived: CarouselNames[nextIndexUp],
-          })
-          break;
+    switch (e.keyCode) {
+      case KeyMap.left:
+        this[this.state.isActived].slickPrev();
+        break;
 
-        case KeyMap.down:
-          const currIndexDown = CarouselNames.findIndex(name => name === this.state.isActived);
-          const nextIndexDown = currIndexDown + 1 > MaxIndex ? MaxIndex : currIndexDown + 1;
-          focusBox.classList = `focus ${CarouselNames[nextIndexDown]}`;
-          this.setState({
-            isActived: CarouselNames[nextIndexDown],
-          })
-          break;
+      case KeyMap.right:
+        this[this.state.isActived].slickNext();
+        break;
 
-        default:
-      }
-    });
+      case KeyMap.up:
+        const nextIndexUp = currIndex - 1 < 0 ? 0 : currIndex - 1;
+        this.focusBox.classList = `focus ${CarouselNames[nextIndexUp]}`;
+        this.setState({
+          isActived: CarouselNames[nextIndexUp],
+        })
+        break;
+
+      case KeyMap.down:
+        const nextIndexDown = currIndex + 1 > MaxIndex ? MaxIndex : currIndex + 1;
+        this.focusBox.classList = `focus ${CarouselNames[nextIndexDown]}`;
+        this.setState({
+          isActived: CarouselNames[nextIndexDown],
+        })
+        break;
+
+      default:
+        console.log('keyCode : ', e.keyCode);
+    }
+  }
+
+  getFocusBoxTop = ({ hasPadding = false } = {}) => {
+    const index = CarouselNames.findIndex(name => name === this.state.isActived);
+    const base = index * (imgHeight + 20 * 2) + (index + 1) * 40;
+    return hasPadding ? base + 20 : base;
   }
 
   render() {
-    const index = CarouselNames.findIndex(name => name === this.state.isActived);
     return (
-      <div className='container' style={{ top: -(index * 240) }}>
+      <div className='container' style={{ top: - this.getFocusBoxTop() }}>
         <Carousel reference={n => this.slider = n} slides={makefakeData('slider')} />
         <Carousel reference={n => this.slider2 = n} slides={makefakeData('slider2')} />
         <Carousel reference={n => this.slider3 = n} slides={makefakeData('slider3')} />
-        <div id="focus-box" className='focus' style={{ top: index * 240 + 20 }} />
+        <div
+          id="focus-box"
+          className='focus'
+          style={{ top: this.getFocusBoxTop({ hasPadding: true }) }}
+        />
       </div>
     );
   }
